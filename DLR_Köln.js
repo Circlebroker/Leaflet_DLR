88449cc73635b75e7c476aa6007dacd9671742a4
Circlebroker <68075309+Circlebroker@users.noreply.github.com>
2020-12-02 15:58:35 +0100

    Initial

---
+++ DLR_Köln.js
@@ -0,0 +1,216 @@
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "generator": "overpass-ide",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2020-12-02T13:23:02Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "relation/1758020",
+        "access": "private",
+        "city": "Köln",
+        "country": "DE",
+        "postcode": "51147",
+        "street": "Linder Höhe",
+        "int_name": "German Aerospace Center",
+        "landuse": "industrial",
+        "name": "Deutsches Zentrum für Luft- und Raumfahrt",
+        "name:en": "German Aerospace Center",
+        "name:es": "Centro Aeroespacial Alemán",
+        "name:fr": "Centre allemand pour l'aéronautique et l'aérospatiale",
+        "name:pl": "Niemiecka Agencja Kosmiczna",
+        "short_name": "DLR",
+        "source": "cc-by-sa/3.0 Lageplan: http://www.dlr.de/Portaldata/1/Resources/standorte/koeln/Lageplan_DLR-Standort_K_ln.pdf",
+        "type": "multipolygon",
+        "website": "https://www.dlr.de/",
+        "wikidata": "Q157332",
+        "wikipedia": "de:Deutsches Zentrum für Luft- und Raumfahrt"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              7.1162318,
+              50.8522485
+            ],
+            [
+              7.1162335,
+              50.8521543
+            ],
+            [
+              7.11621,
+              50.8512124
+            ],
+            [
+              7.119583,
+              50.8511903
+            ],
+            [
+              7.1196953,
+              50.8511864
+            ],
+            [
+              7.1196905,
+              50.8513218
+            ],
+            [
+              7.1219936,
+              50.8513086
+            ],
+            [
+              7.1232887,
+              50.8512901
+            ],
+            [
+              7.1241532,
+              50.8512782
+            ],
+            [
+              7.1242516,
+              50.851277
+            ],
+            [
+              7.1261559,
+              50.8512529
+            ],
+            [
+              7.1284602,
+              50.8512492
+            ],
+            [
+              7.1301666,
+              50.8512306
+            ],
+            [
+              7.1327036,
+              50.8512129
+            ],
+            [
+              7.1329946,
+              50.8516182
+            ],
+            [
+              7.1332028,
+              50.8519383
+            ],
+            [
+              7.1331044,
+              50.8519601
+            ],
+            [
+              7.1331193,
+              50.8520051
+            ],
+            [
+              7.1331276,
+              50.8520332
+            ],
+            [
+              7.1332521,
+              50.8520804
+            ],
+            [
+              7.1332841,
+              50.8526466
+            ],
+            [
+              7.1332753,
+              50.8529398
+            ],
+            [
+              7.1332503,
+              50.8530836
+            ],
+            [
+              7.1331516,
+              50.8532099
+            ],
+            [
+              7.1330315,
+              50.8533391
+            ],
+            [
+              7.1328424,
+              50.8534725
+            ],
+            [
+              7.1326545,
+              50.8535493
+            ],
+            [
+              7.1322877,
+              50.8536417
+            ],
+            [
+              7.1266607,
+              50.8544559
+            ],
+            [
+              7.1238771,
+              50.8548103
+            ],
+            [
+              7.1229342,
+              50.8549391
+            ],
+            [
+              7.1219951,
+              50.8550629
+            ],
+            [
+              7.1200874,
+              50.8552997
+            ],
+            [
+              7.1199771,
+              50.8550854
+            ],
+            [
+              7.1196843,
+              50.8550884
+            ],
+            [
+              7.1196363,
+              50.8550898
+            ],
+            [
+              7.1196382,
+              50.855161
+            ],
+            [
+              7.1194913,
+              50.8551589
+            ],
+            [
+              7.1189168,
+              50.8551522
+            ],
+            [
+              7.1187837,
+              50.8551506
+            ],
+            [
+              7.1187778,
+              50.8545161
+            ],
+            [
+              7.1173636,
+              50.8540844
+            ],
+            [
+              7.1162646,
+              50.8537513
+            ],
+            [
+              7.1162318,
+              50.8522485
+            ]
+          ]
+        ]
+      },
+      "id": "relation/1758020"
+    }
+  ]
+}
